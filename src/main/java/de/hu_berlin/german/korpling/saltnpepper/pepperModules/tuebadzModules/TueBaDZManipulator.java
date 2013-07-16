f9fef745a3cb2e3047b87317bec1086c2adce73b
FlorianZipser <f.zipser@gmx.de>
2013-07-16 19:32:20 +0000

    adopted even TreeTaggerExporter to: - new multithreading architecture
    git-svn-id: https://korpling.german.hu-berlin.de/svn/saltnpepper/PepperModules/TueBaDZModules/trunk@8221 47085195-c50e-4317-8f43-7cb808db0f00

--- src/main/java/de/hu_berlin/german/korpling/saltnpepper/pepperModules/tuebadzModules/TueBaDZManipulator.java
+++ src/main/java/de/hu_berlin/german/korpling/saltnpepper/pepperModules/tuebadzModules/TueBaDZManipulator.java
@@ -19,11 +19,8 @@ package de.hu_berlin.german.korpling.saltnpepper.pepperModules.tuebadzModules;
 
 import org.osgi.service.component.annotations.Component;
 
-import de.hu_berlin.german.korpling.saltnpepper.pepper.pepperExceptions.PepperModuleException;
-import de.hu_berlin.german.korpling.saltnpepper.pepper.pepperModules.PepperManipulator;
+import de.hu_berlin.german.korpling.saltnpepper.pepper.pepperModules.PepperMapper;
 import de.hu_berlin.german.korpling.saltnpepper.pepper.pepperModules.impl.PepperManipulatorImpl;
-import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sCorpusStructure.SDocument;
-import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sDocumentStructure.SDocumentGraph;
 import de.hu_berlin.german.korpling.saltnpepper.salt.saltCore.SElementId;
 
 /**
@@ -46,41 +43,14 @@ public class TueBaDZManipulator extends PepperManipulatorImpl
 		this.name= "TueBaDZManipulator";
 	}
 	
-	
 	/**
-	 * This method is called by method start() of superclass PepperManipulator, if the method was not overriden
-	 * by the current class. If this is not the case, this method will be called for every document which has
-	 * to be processed.
-	 * This method traverses the graph of the {@link SDocumentGraph} object given by the given {@link SElementId} object by top down | 
-	 * depth first algorithm. During the traversion each node will be identified by being either a topological node or a syntactic node. A node
-	 * is identified as topological, when it has an annotation value being contained in the list {@link TueBaDZManipulator#topologicalAnnotations}, 
-	 * all the other nodes are identified as a syntactic node. Each syntactic node and each topologic node as well will be copied in a common
-	 * syntactic graph or topological graph. Both artificional graphs will be added to the {@link SDocumentGraph} object. All nodes of both graphs
-	 * will be marked as syntactic or topological by putting them to a syntactic or morphological layer.
-	 * <br/>
-	 * An artificial root node for all topological nodes and another one for all syntactical nodes will be created.
-	 * <br/>
-	 * If a node is a syntactic layer, the node will be copied and added to the syntactical layer. The gap between to syntactical nodes (as father 
-	 * or sun of a topological node) in the syntactical layer will be filled by copying the relation between sun and topo node to the syntactical layer
-	 * between father and sun.   
-	 * @param sElementId the id value for the current document or corpus to process  
+	 * Creates a mapper of type {@link PAULA2SaltMapper}.
+	 * {@inheritDoc PepperModule#createPepperMapper(SElementId)}
 	 */
 	@Override
-	public void start(SElementId sElementId) throws PepperModuleException 
+	public PepperMapper createPepperMapper(SElementId sElementId)
 	{
-		if (	(sElementId!= null) &&
-				(sElementId.getSIdentifiableElement()!= null) &&
-				((sElementId.getSIdentifiableElement() instanceof SDocument)))
-		{//only if given sElementId belongs to an object of type SDocument or SCorpus	
-			
-			SDocumentGraph sDocGraph= ((SDocument)sElementId.getSIdentifiableElement()).getSDocumentGraph();
-			if(sDocGraph!= null)
-			{//if document contains a document graph
-				Rearanger rearanger= new Rearanger();
-				rearanger.setsDocGraph(sDocGraph);
-				rearanger.start();
-			}//if document contains a document graph
-		}//only if given sElementId belongs to an object of type SDocument or SCorpus
-		
+		Rearanger mapper= new Rearanger();
+		return(mapper);
 	}
 }
